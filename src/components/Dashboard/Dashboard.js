3591ad8932ee2ca8ed14062de70ef239d898ff81
Vlad Shum <shumanskyivlad@gmail.com>
2019-09-20 12:48:52 +0300

    hw_2_bank_account

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Controls from '../Controls/Controls';
+import Balance from '../Balance/Balance';
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
+import styles from './Dashboard.module.css';
+
+export default class Dashboard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      transactions: [],
+      balance: 5000,
+    };
+  }
+
+  handleTransaction = newTransaction => {
+    this.setState(prevState => ({
+      transactions: [...prevState.transactions, newTransaction],
+      balance:
+        newTransaction.type === 'deposit'
+          ? prevState.balance + newTransaction.enteredNumber
+          : prevState.balance - newTransaction.enteredNumber,
+    }));
+  };
+
+  render() {
+    const { balance, transactions } = this.state;
+    return (
+      <div className={styles.dashboard}>
+        <Controls
+          balance={balance}
+          handleTransaction={this.handleTransaction}
+        />
+        <Balance balance={balance} transactions={transactions} />
+        <TransactionHistory transactions={transactions} />
+      </div>
+    );
+  }
+}
